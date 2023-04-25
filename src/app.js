f5f7610dd4e643a666bd7b6b6d9d8e692d3077cc
Fencer1993 <125864763+Fencer1993@users.noreply.github.com>
2023-04-25 09:04:22 +0000

    Agregado el código de javascript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["My pet", "My grandma", "My couple", "My teacher"];
+  let what = ["stole", "throw", "ate", "hide", "kept"];
+  let object = ["my homework", "my car", "my lunch", "my fishes", "my kid"];
+  let where = [
+    "at home",
+    "in the school",
+    "in the library",
+    "in my bedroom",
+    "in the restaurant"
+  ];
+
+  let randomWho = () => {
+    let randomNum = Math.floor(Math.random() * who.length);
+    return who[randomNum];
+  };
+  let randomWhat = () => {
+    let randomNum = Math.floor(Math.random() * what.length);
+    return what[randomNum];
+  };
+  let randomObject = () => {
+    let randomNum = Math.floor(Math.random() * object.length);
+    return object[randomNum];
+  };
+  let randomWhere = () => {
+    let randomNum = Math.floor(Math.random() * where.length);
+    return where[randomNum];
+  };
+
+  let generateExcuse = () => {
+    return `${randomWho} ${randomWhat} ${randomObject} ${randomWhere}!!`;
+  };
+
+  console.log(generateExcuse());
   console.log("Hello Rigo from the console!");
 };
